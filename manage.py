095eb1bc7f3321502eb056382071d8e1a83caa8c
absurdtheory <38075151+absurdtheory@users.noreply.github.com>
2019-10-30 16:50:03 -0400

    updated manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Capstone.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.Capstone.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
