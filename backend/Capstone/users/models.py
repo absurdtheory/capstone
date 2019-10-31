faee6d298b6fffbc050c62a489a3fa5ce6b5fd48
absurdtheory <38075151+absurdtheory@users.noreply.github.com>
2019-10-30 20:00:47 -0400

    added missing line

--- backend/Capstone/users/models.py
+++ backend/Capstone/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 #class CustomUser(AbstractUser):
 #    pass    # Placeholder until we need to add more fields
